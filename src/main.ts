a646231100559ce76157977115e607033fced308
R-Amoedo <126144724+R-Amoedo@users.noreply.github.com>
2023-12-21 20:38:50 -0300

    Adicionada as funções propostas na atividade

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 // Função de Multiplicação
 
+function multiplicar(a: number, b: number) {
+    return a * b;
+}
 
+console.log(multiplicar(5,3))
 
 // Função de Saudação
+
+function saudacaoNome(nome: string) {
+    console.log(`Olá ${nome}.`)
+}
+
+saudacaoNome('Joao');
